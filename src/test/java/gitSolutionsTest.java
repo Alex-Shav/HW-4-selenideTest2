f630428189bc79d28665e5ed9d91394e76a62e36
Alex Shavlyuk <1821413@gmail.com>
2023-03-01 16:28:14 +0300

    add

---
+++ src/test/java/gitSolutionsTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+public class gitSolutionsTest {
+
+    @Test
+    void gitSolutionsTest() {
+        open("https://github.com/");
+        $("div.header-menu-wrapper").$(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("div.enterprise-hero").shouldHave(text("Build like the best"));
+    }
+}
